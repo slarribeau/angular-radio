f83eb726c9933c15adf8e1320464e7c2fc86274c
Scott Larribeau -X (slarribe - LINKS TECHNOLOGY SOLUTIONS INC at Cisco) <slarribe@cisco.com>
2019-02-07 15:55:32 -0800

    first checkin

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import { Item } from '../app/item';
+import { ITEMS } from '../app/mock-data';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  radioSel: any;
+  radioSelected: string;
+  radioSelectedString: string;
+  itemsList: Item[] = ITEMS;
+
+
+  constructor() {
+    this.itemsList = ITEMS;
+    //Selecting Default Radio item here
+    this.radioSelected = "item_3";
+    this.getSelecteditem();
+  }
+
+  // Get row item from array  
+  getSelecteditem() {
+    this.radioSel = ITEMS.find(Item => Item.value === this.radioSelected);
+    this.radioSelectedString = JSON.stringify(this.radioSel);
+  }
+  // Radio Change Event
+  onItemChange(item) {
+    this.getSelecteditem();
+  }
+
+}
